e32fd497d2822d68e543dc1509c9ddf2b58ad805
robertmadril <robertmmadril@gmail.com>
2019-02-21 14:37:53 -0700

    add pseudocode in js file

---
+++ assets/javascript/app.js
@@ -0,0 +1,29 @@
+/*
+
+Pseudocode:
+
+Variables
+
+Functions
+
+Execution
+
+Show home screen
+Start game by clicking start button
+Populate first question with clickable answers and timer starts from x seconds
+{
+If user clicks wrong answer, show answer and image for x seconds, loss count +1. Stop timer.
+If user clicks right answer, show answer and image for x seconds, win count +1. Stop timer.
+else if timer is zero, show answer and image for x seconds, loss count +1. Stop timer.
+
+
+Answer is populated for x seconds before next question is shown. New timer started
+
+}
+Repeat process for all questions
+
+When user reaches end of game stop all timers. Display win, loss, and restart button
+
+Restart button will begin timer and populate first question
+
+*/
